334161575e10e48d54f407cc372e8466a5be5590
Fernando Pérez-García <fepegar@gmail.com>
2025-03-18 12:42:26 +0000

    Add reports

--- src/torchio/datasets/ct_rate.py
+++ src/torchio/datasets/ct_rate.py
@@ -37,12 +37,16 @@ class CtRate(SubjectsDataset):
         token: Optional[str] = None,
         download: bool = False,
         num_subjects: Optional[int] = None,
+        image_key: str = 'image',
+        report_key: str = 'report',
         **kwargs,
     ):
         self._root_dir = Path(root)
         self._token = token
         self._download = download
         self._num_subjects = num_subjects
+        self._image_key = image_key
+        self._report_key = report_key
 
         self._split = self._parse_split(split)
         self._metadata = self._get_metadata()
@@ -57,43 +61,82 @@ class CtRate(SubjectsDataset):
             raise ValueError(f"Invalid split '{split}'. Use 'train' or 'valid'")
         return split
 
+    def _get_csv(
+        self,
+        dirname: str,
+        filename: str,
+        num_subjects: Optional[int] = None,
+    ) -> pd.DataFrame:
+        subfolder = Path(f'dataset/{dirname}')
+        path = Path(self._root_dir, subfolder, filename)
+        if not path.exists():
+            self._download_file_if_needed(path)
+        table = pd.read_csv(path)
+        if num_subjects is not None:
+            table = table.head(num_subjects)
+        table.set_index('VolumeName', inplace=True)
+        return table
+
+    def _get_csv_prefix(self) -> str:
+        if self._split == 'valid':
+            prefix = 'validation'
+        else:
+            prefix = self._split
+        return prefix
+
     def _get_metadata(self) -> pd.DataFrame:
-        subfolder = Path('dataset/metadata')
-        filename = f'{self._split}_metadata.csv'
-        metadata_path = Path(self._root_dir, subfolder, filename)
-        if not metadata_path.exists():
-            self._download_file(metadata_path)
-        metadata = pd.read_csv(metadata_path)
-        if self._num_subjects is not None:
-            metadata = metadata.head(self._num_subjects)
+        dirname = 'metadata'
+        prefix = self._get_csv_prefix()
+        filename = f'{prefix}_metadata.csv'
+        metadata = self._get_csv(dirname, filename, self._num_subjects)
+
+        # Add reports to metadata, keeping only the reports for the images in the
+        # metadata table
+        self._metadata = pd.merge(
+            self._metadata,
+            self._get_reports(),
+            left_index=True,
+            right_index=True,
+            how='left',
+        )
         return metadata
 
-    def _download_file(self, path: Path) -> None:
+    def _get_reports(self) -> pd.DataFrame:
+        dirname = 'radiology_text_reports'
+        prefix = self._get_csv_prefix()
+        filename = f'{prefix}_reports.csv'
+        reports = self._get_csv(dirname, filename)
+        return reports
+
+    def _download_file_if_needed(self, path: Path) -> None:
         if self._download:
-            _check_huggingface_hub()
-            relative_path = path.relative_to(self._root_dir)
-            try:
-                hf_hub_download(
-                    repo_id=self._REPO_ID,
-                    repo_type='dataset',
-                    token=self._token,
-                    subfolder=str(relative_path.parent),
-                    filename=relative_path.name,
-                    local_dir=self._root_dir,
-                )
-            except GatedRepoError as e:
-                message = (
-                    f'The dataset "{self._REPO_ID}" is gated. Visit'
-                    f' https://huggingface.co/datasets/{self._REPO_ID}, accept the'
-                    ' terms and conditions, and log in or create and pass a token to'
-                    ' the `token` argument'
-                )
-                raise RuntimeError(message) from e
+            self._download_file(path)
         else:
             raise FileNotFoundError(
                 f'File "{path}" not found.'
                 " Set 'download=True' to download the dataset"
             )
+
+    def _download_file(self, path: Path) -> None:
+        _check_huggingface_hub()
+        relative_path = path.relative_to(self._root_dir)
+        try:
+            hf_hub_download(
+                repo_id=self._REPO_ID,
+                repo_type='dataset',
+                token=self._token,
+                subfolder=str(relative_path.parent),
+                filename=relative_path.name,
+                local_dir=self._root_dir,
+            )
+        except GatedRepoError as e:
+            message = (
+                f'The dataset "{self._REPO_ID}" is gated. Visit'
+                f' https://huggingface.co/datasets/{self._REPO_ID}, accept the'
+                ' terms and conditions, and log in or create and pass a token to'
+                ' the `token` argument'
+            )
+            raise RuntimeError(message) from e
 
     def _get_subjects_list(self) -> list[Subject]:
         subjects = thread_map(
@@ -104,17 +147,30 @@ class CtRate(SubjectsDataset):
         )
         return subjects
 
-    def _get_subject(self, index_and_row: tuple[int, pd.Series]) -> Subject:
-        _, row = index_and_row
+    def _get_subject(self, filename_and_row: tuple[str, pd.Series]) -> Subject:
+        filename, row = filename_and_row
         subject_dict = row.to_dict()
-        filename = subject_dict.pop('VolumeName')
         image_path = self._root_dir / self._get_image_path(filename)
         if not image_path.exists():
-            self._download_file(image_path)
+            self._download_file_if_needed(image_path)
             self._fix_image(image_path, subject_dict)
         image = ScalarImage(image_path)
-        subject_dict['image'] = image
+        subject_dict[self._image_key] = image
+        report_dict = self._extract_report_dict(subject_dict)
+        subject_dict[self._report_key] = report_dict
         return Subject(**subject_dict)
+
+    def _extract_report_dict(self, subject_dict: dict[str, str]) -> dict[str, str]:
+        report_keys = [
+            'ClinicalInformation_EN',
+            'Findings_EN',
+            'Impressions_EN',
+            'Technique_EN',
+        ]
+        report_dict = {}
+        for key in report_keys:
+            report_dict[key] = subject_dict.pop(key)
+        return report_dict
 
     @staticmethod
     def _get_image_path(filename: str) -> Path:
