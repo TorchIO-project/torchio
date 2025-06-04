d20e6cbcd3d1832b0656e7b1dfe00153080a09b7
Stijn van Wijngaarden <stijn@mriguidance.com>
2025-06-04 15:49:44 +0200

    Add tests for copy argument crop transform

--- tests/transforms/preprocessing/test_crop.py
+++ tests/transforms/preprocessing/test_crop.py
@@ -13,3 +13,17 @@ class TestCrop(TorchioTestCase):
     def test_tensor_multi_channel(self):
         crop = tio.Crop(1)
         assert crop(torch.rand(3, 10, 10, 10)).shape == (3, 8, 8, 8)
+
+    def test_subject_copy(self):
+        crop = tio.Crop(1, copy=True)
+        subject = tio.Subject(t1=tio.ScalarImage(tensor=torch.rand(1, 10, 10, 10)))
+        cropped_subject = crop(subject)
+        assert cropped_subject.t1.shape == (1, 8, 8, 8)
+        assert subject.t1.shape == (1, 10, 10, 10)
+
+    def test_subject_no_copy(self):
+        crop = tio.Crop(1, copy=False)
+        subject = tio.Subject(t1=tio.ScalarImage(tensor=torch.rand(1, 10, 10, 10)))
+        cropped_subject = crop(subject)
+        assert cropped_subject.t1.shape == (1, 8, 8, 8)
+        assert subject.t1.shape == (1, 8, 8, 8)
