d9a82ade198fa09f08d22f3939f853bf0cb4e508
Fernando Pérez-García <fepegar@gmail.com>
2025-06-15 14:24:53 +0100

    Minor edits

--- src/torchio/transforms/preprocessing/spatial/crop.py
+++ src/torchio/transforms/preprocessing/spatial/crop.py
@@ -1,7 +1,7 @@
 from copy import deepcopy
 
-import nibabel as nib
 import numpy as np
+from nibabel.affines import apply_affine
 
 from ....data.image import Image
 from ....data.subject import Subject
@@ -27,12 +27,11 @@ class Crop(BoundsTransform):
             If only one value :math:`n` is provided, then
             :math:`w_{ini} = w_{fin} = h_{ini} = h_{fin}
             = d_{ini} = d_{fin} = n`.
-        copy: bool, optional
-            This transform overwrites the copy argument of the base transform and
-            copies only the cropped patch, instead of the whole image.
-            This can provide a significant speedup when cropping small patches from large images
-            If ``True``, each image will be cropped and the patch copied to a new subject.
-            If ``False``, each image will be cropped in place. Default: ``True``.
+        copy: If ``True``, each image will be cropped and the patch copied to a new
+            subject. If ``False``, each image will be cropped in place. This transform
+            overwrites the copy argument of the base transform and copies only the
+            cropped patch instead of the whole image. This can provide a significant
+            speedup when cropping small patches from large images.
         **kwargs: See :class:`~torchio.transforms.Transform` for additional
             keyword arguments.
 
@@ -41,7 +40,7 @@ class Crop(BoundsTransform):
     """
 
     def __init__(self, cropping: TypeBounds, copy=True, **kwargs):
-        self.copy_patch = copy
+        self._copy_patch = copy
         # Transform base class deepcopies whole subject by default
         # We want to copy only the cropped patch, so we overwrite the functionality
         super().__init__(cropping, copy=False, **kwargs)
@@ -55,12 +54,14 @@ class Crop(BoundsTransform):
         index_ini = low
         index_fin = np.array(subject.spatial_shape) - high
 
-        if self.copy_patch:
+        if self._copy_patch:
             # Create a clean new subject to copy the images into
             # We do this __new__ to avoid calling __init__ so we don't have to specify images immediately
             cropped_subject = subject.__class__.__new__(subject.__class__)
             image_keys_to_crop = subject.get_images_dict(
-                intensity_only=False, include=self.include, exclude=self.exclude
+                intensity_only=False,
+                include=self.include,
+                exclude=self.exclude,
             ).keys()
             keys_to_expose = subject.keys()
             # Copy all attributes we don't want to crop
@@ -75,7 +76,12 @@ class Crop(BoundsTransform):
                     cropped_subject.__dict__[str(key)] = copied_value
                 else:
                     # Images are always exposed, so we don't worry about setting __dict__
-                    cropped_subject[key] = self._crop_image(value, index_ini, index_fin)
+                    cropped_subject[key] = self._crop_image(
+                        value,
+                        index_ini,
+                        index_fin,
+                        copy_patch=self._copy_patch,
+                    )
 
             # Update the __dict__ attribute to include the cropped images
             cropped_subject.update_attributes()
@@ -83,18 +89,26 @@ class Crop(BoundsTransform):
         else:
             # Crop in place
             for image in self.get_images(subject):
-                self._crop_image(image, index_ini, index_fin)
+                self._crop_image(
+                    image,
+                    index_ini,
+                    index_fin,
+                    copy_patch=self._copy_patch,
+                )
             return subject
 
-    def _crop_image(self, image: Image, index_ini: tuple, index_fin: tuple) -> Image:
-        new_origin = nib.affines.apply_affine(image.affine, index_ini)
+    @staticmethod
+    def _crop_image(
+        image: Image, index_ini: tuple, index_fin: tuple, *, copy_patch: bool
+    ) -> Image:
+        new_origin = apply_affine(image.affine, index_ini)
         new_affine = image.affine.copy()
         new_affine[:3, 3] = new_origin
         i0, j0, k0 = index_ini
         i1, j1, k1 = index_fin
 
         # Crop the image data
-        if self.copy_patch:
+        if copy_patch:
             # Create a new image with the cropped data
             cropped_data = image.data[:, i0:i1, j0:j1, k0:k1].clone()
             new_image = type(image)(
