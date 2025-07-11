c8d43c5fb60c1f40b476cb50cd9749345bf9d80e
Fernando Pérez-García <fepegar@gmail.com>
2025-07-11 20:09:46 +0100

    Add a test

--- tests/transforms/preprocessing/test_to.py
+++ tests/transforms/preprocessing/test_to.py
@@ -1,2 +1,17 @@
-def test_todo():
-    assert False
+import torch
+
+import torchio as tio
+
+from ...utils import TorchioTestCase
+
+
+class TestTo(TorchioTestCase):
+    """Tests for :class:`tio.To` class."""
+
+    def test_to(self):
+        transform = tio.To(torch.int)
+        tensor = 10 * torch.rand(2, 3, 4, 5)
+        image = tio.ScalarImage(tensor=tensor)
+        transformed = transform(image)
+        assert image.data.dtype == torch.float32
+        assert transformed.data.dtype == torch.int
