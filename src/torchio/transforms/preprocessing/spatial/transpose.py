4ff705854fd3a67f01c4faaba065cd5290265da4
Fernando Pérez-García <fepegar@gmail.com>
2025-07-19 10:23:11 +0100

    Make things even clearer

--- src/torchio/transforms/preprocessing/spatial/transpose.py
+++ src/torchio/transforms/preprocessing/spatial/transpose.py
@@ -4,7 +4,10 @@ from .to_orientation import ToOrientation
 
 
 class Transpose(SpatialTransform):
-    """Swap the first and last spatial dimensions of the image, respecting metadata.
+    """Swap the first and last spatial dimensions of the image.
+
+    The spatial metadata is updated accordingly, so the world coordinates of
+    all voxels in the input and output spaces match.
 
     Example:
 
@@ -20,7 +23,9 @@ class Transpose(SpatialTransform):
 
     def apply_transform(self, subject: Subject) -> Subject:
         for image in self.get_images(subject):
-            transform = ToOrientation(image.orientation_str[::-1])
+            old_orientation = image.orientation_str
+            new_orientation = old_orientation[::-1]
+            transform = ToOrientation(new_orientation)
             transposed = transform(image)
             image.set_data(transposed.data)
             image.affine = transposed.affine
