67b3ee06f0f536c197e6a04d5abc2c95bd628f3a
Markus J. Ankenbrand <markus.ankenbrand@uni-wuerzburg.de>
2025-11-24 15:56:28 +0100

    Add test for min_constraint

--- tests/transforms/test_transforms.py
+++ tests/transforms/test_transforms.py
@@ -253,6 +253,10 @@ class TestTransform(TorchioTestCase):
         with pytest.raises(ValueError):
             transform.arguments_are_dict()
 
+    def test_min_constraint(self):
+        transform = tio.RandomNoise()
+        assert transform._parse_range(3, 'name', min_constraint=0) == (0,3)
+
     def test_bad_over_max(self):
         transform = tio.RandomNoise()
         with pytest.raises(ValueError):
