e1174f2fb0556fe6b406873c5d23f366665a2ae4
ANIL KUMAR MONDRU <anilkumarmondru@gmail.com>
2025-12-05 12:12:25 +0530

    Add RandomBiasFieldDenoise transform and fix pre-commit issues

--- src/torchio/transforms/augmentation/intensity/random_biasfield_denoise.py
+++ src/torchio/transforms/augmentation/intensity/random_biasfield_denoise.py
@@ -1,10 +1,7 @@
 from torchio.data.subject import Subject
 
-<<<<<<< HEAD
-=======
 from ...transform import Transform
 
->>>>>>> e8e8035 (Apply Ruff autofix and sort imports)
 
 class RandomBiasFieldDenoise(Transform):
     """
